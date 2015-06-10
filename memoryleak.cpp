88700f5e1cf5ddd239d2467d1d50f2dcd747a2b9
y_jono <pek9dkl@gmail.com>
2015-06-10 21:59:47 +0900

    add func() function memoryleak sample.

--- memoryleak.cpp
+++ memoryleak.cpp
@@ -1,8 +1,12 @@
 class Hoge{ private: int a; };
 
-int main() {
-  for(;;){
+void func() {
     Hoge* hoge = new Hoge();
     int* array = new int[1000];
+}
+
+int main() {
+  for(;;){
+    func();
   }
 }
